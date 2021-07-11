4535c8efd39ce404bb55bc2223767c51892d677e
T. Lanette <TLPollard4516@gmail.com>
2021-07-11 12:45:46 -0400

    Updates: styling jumbotron buttons
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/sections/About.js
+++ src/sections/About.js
@@ -3,8 +3,10 @@ import Jumbotron from 'react-bootstrap/Jumbotron';import Container from 'react-b
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 
 import jumbotronPic from '../images/blackwomancodingsmall.jpg';
+import profilePic from '../images/TLPProfile.JPG';
 import '../style/App.css';
 
 const About = () => {
@@ -14,8 +16,18 @@ const About = () => {
                 <Jumbotron fluid className="mt-5">
                     <Row>
                         <Col className="about-info col-12 col-md-6 p-5">
-                            <h1 className="jumbotron-heading">Who am I?</h1>
+                            <h1 className="jumbotron-heading">Hi, I'm Trista!</h1>
+                            <h3 className="jumbotron-subtitle">I love to build amazing websites! </h3>
                             <p className="jumbotron-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer libero tellus, lacinia et ornare sit amet, tincidunt vitae arcu. Vivamus fermentum consequat mi consectetur dapibus. Donec nec ante fermentum, congue mauris porta, molestie lectus. Quisque nec interdum ex. Integer facilisis elit ante, ut consequat mi elementum non. Duis mattis malesuada lorem, vitae auctor lectus hendrerit sed. </p>
+                            <Row>
+                                <Col m-auto justify-content-center>
+                                <Image src={profilePic} m-auto className=" profile-pic" />
+                                </Col>
+                                <Col className="d-grid gap-2 align-items-center">
+                                <Button variant="light" size="lg" className="jumbotron-button"><a href="#contact">Work with Me</a></Button>
+                                <Button variant="light" size="lg" className="jumbotron-button"><a href="#projects">See My Past Work</a></Button>
+                                </Col>
+                            </Row>
                         </Col>
                    
                         <Col>
