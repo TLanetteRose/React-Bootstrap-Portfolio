8fb7c8ff53654f8baaf3d7bf7e57ba1af29bd297
T. Lanette <TLPollard4516@gmail.com>
2021-07-11 11:03:10 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import {Link} from 'react-router-dom';
 
 import logoPic from '../images/RebelCowSm.jpg';
 
@@ -12,20 +11,21 @@ import logoPic from '../images/RebelCowSm.jpg';
 const CustomNavbar = () => {
     return  (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">
                         <Image src={logoPic} className="logo" fluid />
                     </Navbar.Brand>
+                    <h3 className="profile-name justify-center">Trista L. Pollard</h3>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link as={Link}href="#about" className="nav-link">About
+                            <Nav.Link href="#about" className="nav-link">About
                             </Nav.Link>
-                            <Nav.Link as={Link} href="#projects" className="nav-link">Projects</Nav.Link>
-                            <Nav.Link as={Link} href="#skills" className="nav-link">Skills
+                            <Nav.Link href="#projects" className="nav-link">Projects</Nav.Link>
+                            <Nav.Link href="#skills" className="nav-link">Skills
                             </Nav.Link>
-                            <Nav.Link as={Link} href="#contacts" className="nav-link">Contacts
+                            <Nav.Link href="#contacts" className="nav-link">Contacts
                             </Nav.Link>.
                         </Nav>
                     </Navbar.Collapse>
--- src/sections/About.js
+++ src/sections/About.js
@@ -13,16 +13,15 @@ const About = () => {
             <Container fluid className="container">
                 <Jumbotron fluid>
                     <Row>
-                        <Col>
+                        <Col className="about-info col-12 col-md-6">
                             <h2>Who am I?</h2>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer libero tellus, lacinia et ornare sit amet, tincidunt vitae arcu. Vivamus fermentum consequat mi consectetur dapibus. Donec nec ante fermentum, congue mauris porta, molestie lectus. Quisque nec interdum ex. Integer facilisis elit ante, ut consequat mi elementum non. Duis mattis malesuada lorem, vitae auctor lectus hendrerit sed. </p>
                         </Col>
-                    </Row>
-                    <Image src={jumbotronPic} fluid className="header-pic mx-auto my-auto p-5 d-block" />
+                   
+                        <Col>
+                            <Image src={jumbotronPic} fluid className="header-pic mx-auto my-auto p-2 d-block col-12 col-md-6" />
+                        </Col>
+                     </Row>
                 </Jumbotron>
             </Container>
         </>
