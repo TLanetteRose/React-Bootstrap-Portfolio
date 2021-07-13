e44ee44c87afbac5d51b045e01eab097f5e7795b
T. Lanette <TLPollard4516@gmail.com>
2021-07-13 19:47:18 -0400

    Updates: positioning elements
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/sections/About.js
+++ src/sections/About.js
@@ -18,13 +18,13 @@ const About = () => {
                 <Container fluid className="container">
                     <Jumbotron fluid className="jumbotron__container">
                         <Row>
-                            <Col className="about-info col-12 col-lg-6 p-5">
+                            <Col className="about-info col-12 col-lg-6 px-5">
                                 <h1 className="jumbotron-heading heading-dark">Hi, I'm Trista!</h1>
                                 <h3 className="jumbotron-subtitle heading-dark">I love to build amazing websites! </h3>
                                 <p className="jumbotron-text text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer libero tellus, lacinia et ornare sit amet, tincidunt vitae arcu. Vivamus fermentum consequat mi consectetur dapibus. Donec nec ante fermentum, congue mauris porta, molestie lectus. Quisque nec interdum ex. Integer facilisis elit ante, ut consequat mi elementum non. Duis mattis malesuada lorem, vitae auctor lectus hendrerit sed. </p>
                                 <Row>
                                     <Col className="mx-auto">
-                                        <Image src={profilePic} m-auto className=" profile-pic" />
+                                        <Image src={profilePic}  className="profile-pic" />
                                     </Col>
                                     <Col className="d-grid gap-2 align-items-center justify-content-center">
                                         <Button variant="light" size="lg" className="jumbotron-button btn-dark"><a href="#contact" className="text-light">Work with Me</a></Button>
@@ -33,7 +33,7 @@ const About = () => {
                                 </Row>
                             </Col>
                    
-                            <Col>
+                            <Col className="px-5">
                                 <Image src={jumbotronPic} fluid className="header-pic col-12 col-lg-6" />
                             </Col>
                         </Row>
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -10,7 +10,7 @@ const Skills = () => {
         <>
             <section className="skills__section mx-auto w-100">
                 <Container fluid className="skills__container">
-                    <div className="d-flex flex-column mb-2 section__header">
+                    <div className="d-flex flex-column mx-auto mb-2 section__header">
                         <ClipboardListIcon className="mx-auto inline-block mb-4 section__header-icon"/> 
                          <h1 className="mx-auto d-inline-block skills__section-title text-center w-100">My Skills</h1>
                         <p className="skills-text text-dark fs-3"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam fuga dolore.</p>
